d248a9dc69986bc2b5b12de8dc8612a6776eeed0
inflaton <inflaton.sg@gmail.com>
2024-06-18 22:34:37 +0800

    get ready for running on GPU cluster

---
+++ tune.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+BASEDIR=$(dirname "$0")
+cd $BASEDIR
+echo Current Directory:
+pwd
+
+nvidia-smi
+uname -a
+cat /etc/os-release
+lscpu
+grep MemTotal /proc/meminfo
+
+pip install -r requirements.txt
+FLASH_ATTENTION_FORCE_BUILD=TRUE pip install --quiet --upgrade flash-attn
+
+export MODEL_NAME=unsloth/Qwen2-7B-Instruct
+echo Tuning $MODEL_NAME
+python tune.py
+
+export MODEL_NAME=gradientai/Llama-3-8B-Instruct-Gradient-1048k
+echo Tuning $MODEL_NAME
+python tune.py
