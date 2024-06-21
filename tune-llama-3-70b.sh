291ba9c123d3cd247e814aac5f0a3ddbf970a49a
Donghao Huang <inflaton.sg@gmail.com>
2024-06-22 06:58:01 +0800

    completed Qwen2-7B-Instruct

--- tune-llama-3-70b.sh
+++ tune-llama-3-70b.sh
@@ -14,6 +14,10 @@ grep MemTotal /proc/meminfo
 # pip install -r requirements.txt
 # FLASH_ATTENTION_FORCE_BUILD=TRUE pip install --upgrade flash-attn
 
+export MODEL_NAME=unsloth/Qwen2-72B-Instruct-bnb-4bit
+echo Tuning $MODEL_NAME
+python tune.py
+
 export MODEL_NAME=unsloth/llama-3-70b-Instruct-bnb-4bit
 echo Tuning $MODEL_NAME
 python tune.py
--- tune-mistral.sh
+++ tune-mistral.sh
@@ -14,7 +14,11 @@ grep MemTotal /proc/meminfo
 # pip install -r requirements.txt
 # FLASH_ATTENTION_FORCE_BUILD=TRUE pip install --upgrade flash-attn
 
-export MODEL_NAME=unsloth/mistral-7b-v0.3-bnb-4bit
+export MODEL_NAME=unsloth/Qwen2-7B-Instruct
+echo Tuning $MODEL_NAME
+python tune.py
+
+export MODEL_NAME=unsloth/mistral-7b-instruct-v0.3
 echo Tuning $MODEL_NAME
 python tune.py
 
