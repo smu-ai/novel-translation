48931465dddf0678ef583a131e1e83a38b1e6d3e
inflaton <inflaton.sg@gmail.com>
2024-06-23 10:18:31 +0800

    Update tune-medium.sh

--- tune-medium.sh
+++ tune-medium.sh
@@ -14,11 +14,14 @@ grep MemTotal /proc/meminfo
 # pip install -r requirements.txt
 # FLASH_ATTENTION_FORCE_BUILD=TRUE pip install --upgrade flash-attn
 
+export MODEL_NAME=unsloth/Qwen2-7B-Instruct
+echo Tuning $MODEL_NAME
+python tune.py
+
+export MODEL_NAME=unsloth/mistral-7b-instruct-v0.3
+echo Tuning $MODEL_NAME
+python tune.py
+
 export MODEL_NAME=gradientai/Llama-3-8B-Instruct-Gradient-1048k
 echo Tuning $MODEL_NAME
 python tune.py
-
-export MODEL_NAME=unsloth/Qwen2-7B-Instruct-bnb-4bit
-echo Tuning $MODEL_NAME
-python tune.py
-
